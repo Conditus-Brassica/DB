fefab516561987ea657e0eb895e84bd662156059
St. Kek O'Paris <stas.voitkus2003@gmail.com>
2024-07-31 16:39:38 +0300

    fix

--- wait_db.sh
+++ wait_db.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
-until [[ ! -n $(ss -tulpn | grep -E ":7678[[:space:]]" | grep -o "LISTEN") ]]; do
+until [ ! -n $(ss -tulpn | grep -E ":7678[[:space:]]" | grep -o "LISTEN") ]; do
     sleep 0.1;
 done;
