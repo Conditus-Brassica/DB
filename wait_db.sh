986d166ec500a3be94c051d02344d4307d665c68
St. Kek O'Paris <stas.voitkus2003@gmail.com>
2024-07-31 16:30:23 +0300

    fix script

--- wait_db.sh
+++ wait_db.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
-until [ "`docker inspect -f {{.State.Running}} CONTAINERNAME`"=="true" ]; do
+until [[ ! -n $(ss -tulpn | grep -E ":7678[[:space:]]" | grep -o "LISTEN") ]]; do
     sleep 0.1;
 done;
