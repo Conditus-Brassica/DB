7459dfe9213b656f1d56e1480305093e005b5712
St. Kek O'Paris <stas.voitkus2003@gmail.com>
2024-07-31 20:22:27 +0300

    fix

--- neo4j_installation_script.sh
+++ neo4j_installation_script.sh
@@ -5,5 +5,5 @@ wget -O - https://debian.neo4j.com/neotechnology.gpg.key | gpg --dearmor -o /etc
 echo 'deb https://debian.neo4j.com stable latest' | tee /etc/apt/sources.list.d/neo4j.list
 apt-get update
 
-apt-get install neo4j=1:5.18.0 -y
+apt-get install neo4j=1:5.18.0 -y 8 29
 
